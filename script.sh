6383cc9fef115e28631cebc6ce551696ffe42152
Ariful islam <arif98741@gmail.com>
2021-09-28 10:35:59 +0600

    change

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@
 # This file is built for root user not others. To modify for others users , simply add 'sudo' as prefix
 # of every command
 # Issues and Security: This file is built for only single use. You are also free to use.
-# You are fully responsible for if any damage happend due to use of this bash script. So, use it
+# You are fully responsible for if any damage happened due to use of this bash script. So, use it
 # at your own risk
 
 function run_script() {
@@ -25,6 +25,8 @@ function run_script() {
     project='something.com'                            #your project name. you should change it according to your domain name
     clone_dir="/var/www/$project"                      #where to clone . no need to change
     clone_url='git@github.com:arif98741/something.git' #your git repository url example: git@url.com:user/repo.git
+    server='nginx'
+    php_version=4 # here 4 refers to 7.4; In same way 3 refers to 7.3
     permission_755=755
     permission_756=756
     permission_775=775
@@ -39,8 +41,9 @@ function run_script() {
     echo
 
     echo 'https://github.com/settings/keys'
-    echo \
-      ssh-keygen -y
+
+    echo
+    ssh-keygen -y
     cat ~/.ssh/id_rsa.pub
     echo
 
@@ -48,13 +51,34 @@ function run_script() {
     git clone $clone_url $clone_dir
 
     #if else condition for running further step
+
+    # shellcheck disable=SC2162
     read -p "Have you copied and paste ssh key to github, yes/no: " decision
-    if [ $decision = 'yes' ]; then
+
+    if [ "$decision" = 'yes' ]; then
       echo "Excellent Boss!. "
       echo
 
       echo 'Running further script like nginx and so on ....'
       echo
+
+      echo "=========================================================="
+      echo "Step2: PHP Installation"
+      echo "============================================================"
+
+      # shellcheck disable=SC2162
+      read -p "Do you want to install php, yes/no: " decision
+      if [ "$decision" = 'yes' ]; then
+        echo "Installing PHP"
+        echo
+
+        # shellcheck disable=SC2091
+        $(php_installation $php_version)
+
+        echo "Successfully Installed PHP 7.$php_version"
+        echo
+
+      fi
 
       echo "=========================================================="
       echo "Step2: Nginx Installation"
@@ -99,7 +123,8 @@ function run_script() {
       echo
 
       echo "Adding Permission directory"
-      chown -R $USER:$USER $clone_dir
+      # shellcheck disable=SC2086
+      chown -R $USER:"$USER" $clone_dir
 
       echo
 
@@ -107,7 +132,8 @@ function run_script() {
       echo
 
       echo "Permission for $clone_dir"
-      chown -R $USER:$USER "/var/www/html"
+      # shellcheck disable=SC2086
+      chown -R $USER:"$USER" "/var/www/html"
       chmod -R $permission_755 $clone_dir
       echo
 
@@ -120,14 +146,16 @@ function run_script() {
       echo "============================================================"
       echo
 
-      read -p "Do you want to create create server block " decision
-      if [ $decision = 'yes' ]; then
+      # shellcheck disable=SC2162
+      read -p "Do you want to manage nginx server block " decision
+      if [ "$decision" = 'yes' ]; then
         echo \
           echo 'Creating server block'
         echo
 
+        # shellcheck disable=SC2162
         read -p "Do you want to copy pre-built default file? yes/no: " copydecision
-        if [ $copydecision = 'yes' ]; then #this will handle wheather new file will be created or
+        if [ "$copydecision" = 'yes' ]; then #this will handle wheather new file will be created or
           #nano /var/www/etc
           cp /etc/nginx/sites-available/default /etc/nginx/sites-available/$project
           echo "New server block added $project"
@@ -140,21 +168,32 @@ function run_script() {
         #nginx status
         nginx -t
 
+        # shellcheck disable=SC2162
         read -p "Have you done editing? yes/no: " done_editing
-        if [ $done_editing = 'yes' ]; then
+        if [ "$done_editing" = 'yes' ]; then
           echo "Congratulations you have done editing configuration file"
         fi
         systemctl restart nginx
       else
-        echo 'Server Block Creation ignored!'
+        echo 'Server Block Management ignored!'
       fi
 
       echo 'Script Execution Successfully Completed'
     fi
 
   else
-    echo "Could not run script on $current_dir directory. Run this script in /var/www"
+    echo "Could not run script on this directory. Run this script in /var/www"
   fi
+}
+
+# install php
+function php_installation() { #accepts one param version. don't change here. Change from php_version at the top of this script
+  apt-get install php7."$1" -y
+  # shellcheck disable=SC2086
+  apt-get install php7.$1-fpm php7.$1-cli php7.$1-mysql php7.$1-curl php7.$1-json -y
+  systemctl start php7."$1"-fpm
+  # shellcheck disable=SC2086
+  systemctl enable php7.$1-fpm
 }
 
 run_script
